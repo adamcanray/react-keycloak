742773230d4f2c4c7636fdfb777fa1933e741850
adamcanray <adamcool180@gmail.com>
2022-09-02 00:53:39 +0700

    setup env config

--- src/Keycloak.js
+++ src/Keycloak.js
@@ -1,10 +1,10 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://192.168.81.170:8888/auth", // keycloak 18 or older
+  url: process.env.REACT_APP_KC_HOST + "/auth", // keycloak 18 or older
   //   url: "http://localhost:8888/", // keycloak 19
-  realm: "keycloak-react-auth",
-  clientId: "react-auth",
+  realm: process.env.REACT_APP_KC_REALM,
+  clientId: process.env.REACT_APP_KC_CLIENT_ID,
 });
 
 export default keycloak;
