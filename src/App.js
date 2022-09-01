e8cd314c11564d0c6ddcb7fb098e6ac272b959f0
adamcanray <adamcool180@gmail.com>
2022-09-01 23:45:13 +0700

    setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -17,6 +17,9 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="text-3xl font-bold underline text-red-100">
+          Hello world!
+        </h1>
       </header>
     </div>
   );
